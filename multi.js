430da2c91e34af21b37d47505e5ff6dcde32ad86
dayframs <dayframs09@yahoo.com>
2023-09-21 17:13:30 +0100

    JS logic updated

--- multi.js
+++ multi.js
@@ -1,13 +1,30 @@
 function multiply() {
-   i = inp1.value;
+    i = inp1.value;
     j = inp2.value;
     k = result.innerText;
-for (let i = 1; i <= inp1.value; i++){
-    for (let j = 1; j <= inp2.value; j++){
-        result.innerText +=
-            (i + '*' + j +'='+ (i*j));
-        console.log(i + '*' + j +'='+ (i*j));
-    }
+    l = true;
+    result.innerText = '';
+    start = false;
+
+    if (i && j) {
+        start = true;
+    } else { alert('Fill in your numbers') }
+    
+
+        if (start) {
+            for (let i = 1; i <= inp1.value; i++) {
+                for (let j = 1; j <= inp2.value; j++) {
+                    result.innerText +=
+                        (i + '*' + j + '=' + (i * j));
+                    console.log(i + '*' + j + '=' + (i * j));
+                }
+            }
+        }
 }
+function reset() {
+    inp1.value = '';
+    inp2.value = '';
+    result.innerText = '';
+    start = false;
 }
 
